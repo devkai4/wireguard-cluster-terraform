cc1ef4c3274025c9320a446f8a14f62f9e33a4e8
devkai4 <devkai4@pm.me>
2025-05-04 22:17:15 +0900

    fix: correct S3 versioning status from 'Enable' to 'Enabled'

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket" "terraform_state" {
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
-    status = "Enable"
+    status = "Enabled"
   }
 }
 
