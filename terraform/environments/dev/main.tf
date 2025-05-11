3f21612f728048167ec3ab77f42f663f176c2e65
devkai <devkai4@pm.me>
2025-05-11 21:47:00 +0900

    Add basic WireGuard setup with Ansible

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,11 +1,10 @@
-
 # Use EC2 Module for VPN Server
 module "vpn_server" {
   source = "../../modules/ec2"
   
   # Only create in dev environment for testing
   count = var.environment == "dev" ? 1 : 0
-
+  
   # EC2 Configuration
   instance_name       = "${var.project}-${var.environment}-vpn-server"
   ami_id              = var.vpn_server_ami_id
@@ -17,23 +16,24 @@ module "vpn_server" {
   associate_public_ip = true
   root_volume_size    = 20
   enable_monitoring   = true
-
+  
   # IAM Role Configuration
   create_iam_role          = true
   enable_ssm_session_manager = true
   iam_policies             = [
     "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   ]
-
+  
   # User Data Configuration
   user_data_vars = {
     hostname                = "${var.project}-${var.environment}-vpn-server"
     install_cloudwatch_agent = "true"
     install_node_exporter   = "true"
+    install_wireguard       = "true" 
     log_group_name          = "/vpn-cluster/${var.environment}/vpn-server"
     additional_user_data    = ""
   }
-
+  
   # Tags
   environment = var.environment
   project     = var.project
--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,9 +1,33 @@
+provider "aws" {
+  region  = var.region
+  profile = var.aws_profile
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-1"  # Tokyo region
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile to use"
+  type        = string
+  default     = "vpn-project"
+}
 
 # EC2 Variables
+# AMI ID for Tokyo region (ap-northeast-1)
 variable "vpn_server_ami_id" {
-  description = "AMI ID for the VPN server (Ubuntu 22.04 LTS)"
+  description = "AMI ID for the VPN server (Ubuntu 22.04 LTS in Tokyo region)"
   type        = string
-  default     = "ami-0ed99df77a82560e6"  # Ubuntu 22.04 LTS in us-east-1 (update for your region)
+  default     = "ami-0bc4853c891c551f8"
+}
+
+# Availability Zones for Tokyo region
+variable "availability_zones" {
+  description = "List of availability zones in Tokyo region"
+  type        = list(string)
+  default     = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
 }
 
 variable "vpn_server_instance_type" {
@@ -16,4 +40,34 @@ variable "ssh_key_name" {
   description = "SSH key name for the EC2 instance"
   type        = string
   default     = null  # Will need to be set in terraform.tfvars
+}
+# WireGuard Variables
+variable "wireguard_port" {
+  description = "UDP port for WireGuard"
+  type        = number
+  default     = 51820
+}
+
+variable "wireguard_network" {
+  description = "Internal network CIDR for WireGuard"
+  type        = string
+  default     = "10.8.0.0/24"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "vpn-cluster"
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "owner" {
+  description = "Owner of the resources"
+  type        = string
+  default     = "devkai4"
 }
